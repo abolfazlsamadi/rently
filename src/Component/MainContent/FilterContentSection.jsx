bba9cf44bf303337e1d1d54cc0bd78e61cfc36ad
abolfazl <a.abolfazlsamadi1378@gmail.com>
2023-12-06 10:52:45 +0330

    coding filter content and create place data

--- src/Component/MainContent/FilterContentSection.jsx
+++ src/Component/MainContent/FilterContentSection.jsx
@@ -1,6 +1,13 @@
+import { AdjustmentsHorizontalIcon } from "@heroicons/react/24/outline";
+
 const FilterContentSection = () => {
     return ( 
-        <div className="main-content__top-main__filter-container"></div>
+        <div className="main-content__top-main__filter-container">
+            <div className="main-content__top-main__filter-container__icon-container">
+                <AdjustmentsHorizontalIcon className="main-content__top-main__filter-container__icon-container__icon" />
+            </div>
+            <div  className="main-content__top-main__filter-container__text">مرتب سازی</div>
+        </div>
      );
 }
  
